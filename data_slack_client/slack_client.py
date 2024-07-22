58ea030deba2c721939c2f98654764b1dea5e59c
Humaid Mollah <73120385+hnm27@users.noreply.github.com>
2024-07-22 16:13:17 +0200

    add functionality to add as many stakeholders as required in class arg

--- data_slack_client/slack_client.py
+++ data_slack_client/slack_client.py
@@ -1,4 +1,5 @@
 """Client to interact with Slack."""
+
 from __future__ import annotations
 
 import random
@@ -21,10 +22,14 @@ LOGGER = structlog.get_logger()
 class SlackClient:
     """Client used to interact with Slack."""
 
-    def __init__(self, config: SlackConfig,
-                 slack_channel: str,
-                 init_block: bool = True,
-                 header: Union[str, None] = None):
+    def __init__(
+        self,
+        config: SlackConfig,
+        slack_channel: str,
+        init_block: bool = True,
+        header: Union[str, None] = None,
+        stakeholders: dict = {},
+    ):
         """
         Initialize the Slack Client.
 
@@ -36,18 +41,24 @@ class SlackClient:
             init_block: True if you want to add an introduction block
                         containing a python-job-name,date and time; else False.
             header: Name of Python-job if init_block is True, else None.
+            stakeholders (dict[str, str]): A dictionary with stakeholder names as keys and member IDs as values.
         """
+        assert isinstance(stakeholders, dict)
+        assert isinstance(config, SlackConfig)
         self.config = config
         self.slack_channel = slack_channel
         self.header = header
+        self.stakeholders = stakeholders
         self.blocks: list[dict] = []
         self.response: dict[Any, Any] | SlackResponse = {}
         try:
             self.slack_client = WebClient(token=self.config.bot_token)
             log("Successfully initialized Slack WebClient")
         except SlackApiError as e:
-            log_and_raise_error(f"Could not initialize SlackClient. "
-                                f"Error: {e.response.get('error', 'No error info in response')}.")
+            log_and_raise_error(
+                f"Could not initialize SlackClient. "
+                f"Error: {e.response.get('error', 'No error info in response')}."
+            )
         if init_block is True:
             assert header is not None
             self.initialize_block_message(header_name=header)
@@ -62,21 +73,32 @@ class SlackClient:
         """
         if e.response.status_code == 429:
             retry_after = int(e.response.headers.get("Retry-After", 1))
-            backoff_time = min(retry_after, 30) + random.uniform(0, 10)  # Calculate exponential backoff time and Add jitter
+            backoff_time = min(retry_after, 30) + random.uniform(
+                0, 10
+            )  # Calculate exponential backoff time and Add jitter
             log(f"Rate limit exceeded. Retrying after {backoff_time} seconds.")
             time.sleep(backoff_time)
         else:
-            log_and_raise_error(f"Slack API error: {e.response.get('error', 'No error info in response')}.")
+            log_and_raise_error(
+                f"Slack API error: {e.response.get('error', 'No error info in response')}."
+            )
 
-    def post_simple_message(self, message: str, slack_channel: str = None) -> None:
+    def post_simple_message(
+        self, message: str, slack_channel: str = None, tag_stakeholders: bool = False
+    ) -> None:
         """
         Post a simple message on Slack.
 
         Args:
-             message: plain text message.
-             slack_channel [Optional]: Name of Slack-Channel to write to.
+            message (str): Plain text message.
+            slack_channel (str, optional): Name of Slack channel to write to. Defaults to None.
+            tag_stakeholders (bool, optional): Whether to tag stakeholders in the message. Defaults to False.
         """
-        assert type(message) == str
+        assert isinstance(message, str)
+
+        if tag_stakeholders and self.stakeholders:
+            message += "\n" + self.build_tag_stakeholder_message()
+
         try:
             self.slack_client.chat_postMessage(
                 channel=f"#{slack_channel or self.slack_channel}", text=message
@@ -86,38 +108,40 @@ class SlackClient:
             self.handle_slack_api_error(e)
 
     def send_secret_message_in_channel(
-            self,
-            message: str,
-            user: str | None = None,
-            slack_channel: str = None
+        self, message: str, user_member_id: str | None = None, slack_channel: str = None
     ) -> None:
         """
         Post a secret message on Slack to a particular user.
 
         Args:
              message: plain text message.
-             user: user id (member id).
-             slack_channel [Optional]: Name of Slack-Channel to write to. 
+             user_member_id: user member id
+             slack_channel [Optional]: Name of Slack-Channel to write to.
         """
-        assert type(message) == str and type(user) == str
+        assert isinstance(message, str) and isinstance(user_member_id, str)
         try:
             self.slack_client.chat_postEphemeral(
-                channel=f"#{slack_channel or self.slack_channel}", text=message, user=user
+                channel=f"#{slack_channel or self.slack_channel}",
+                text=message,
+                user=user_member_id,
             )
             log("Successfully posted secret message")
         except SlackApiError as e:
             self.handle_slack_api_error(e)
 
-    def send_block_message(self, slack_channel: str = None, blocks: list[dict] = None) -> None:
+    def send_block_message(
+        self, slack_channel: str = None, blocks: list[dict] = None
+    ) -> None:
         """Send a block message on Slack and store the response.
-        
+
         Args:
-             slack_channel [Optional]: Name of Slack-Channel to write to. 
+             slack_channel [Optional]: Name of Slack-Channel to write to.
              blocks: [Optional]: Slack message block object.
         """
         try:
             self.response = self.slack_client.chat_postMessage(
-                channel=f"#{slack_channel or self.slack_channel}", blocks=blocks or self.blocks
+                channel=f"#{slack_channel or self.slack_channel}",
+                blocks=blocks or self.blocks,
             )
             log("Successfully sent message block")
         except SlackApiError as e:
@@ -135,143 +159,144 @@ class SlackClient:
         except SlackApiError as e:
             self.handle_slack_api_error(e)
         except KeyError as e:
-            log_and_raise_error(f"Key error: {e.response.get('error', 'No error info in response')}.")
+            log_and_raise_error(
+                f"Key error: {e.response.get('error', 'No error info in response')}."
+            )
 
     def initialize_block_message(self, header_name: str) -> None:
         """
         Initialize Slack block message and send it to Slack.
 
         Args:
-             header_name: Block message Header Name (Eg. Salesforce-Updater).
+            header_name (str): Block message Header Name (e.g., Salesforce-Updater).
         """
-        assert type(header_name) == str
+        assert isinstance(header_name, str)
         amsterdam_tz = pytz.timezone("Europe/Amsterdam")
-        self.blocks = [
+        block_elements = [
             {
                 "type": "context",
                 "elements": [
                     {
                         "type": "mrkdwn",
                         "text": f"Invoking *{header_name}* :on: \n"
-                                f"Date: {datetime.now(amsterdam_tz).strftime('%Y-%m-%d')} \t"
-                                f"Time: {datetime.now(amsterdam_tz).time()}",
+                        f"Date: {datetime.now(amsterdam_tz).strftime('%Y-%m-%d')} \t"
+                        f"Time: {datetime.now(amsterdam_tz).time()}",
                     }
                 ],
             },
             {"type": "divider"},
         ]
+        self.blocks = block_elements
         self.send_block_message()
 
     def add_message_block(
-            self,
-            message: str,
-            img_url: str | None = None,
-            img_alt_text: str = "",
-            temp: bool = False,
+        self,
+        message: str,
+        img_url: str | None = None,
+        img_alt_text: str = "",
+        temp: bool = False,
+        tag_stakeholders: bool = False,
     ) -> None:
         """
         Add a message to the Slack block message.
 
         Args:
-            message: plain text message.
-            img_url: url of the image to add to the message [Optional].
-            img_alt_text: alt text for image [Optional].
-            temp: True if this is a temporary message.
+            message (str): Plain text message.
+            img_url (str, optional): URL of the image to add to the message. Defaults to None.
+            img_alt_text (str, optional): Alt text for image. Defaults to "".
+            temp (bool, optional): True if this is a temporary message. Defaults to False.
+            tag_stakeholders (bool, optional): Whether to tag stakeholders in the message. Defaults to False.
         """
+        block_elements = [{"type": "mrkdwn", "text": message}]
+
         if img_url is not None:
-            self.blocks.append(
-                {
-                    "type": "context",
-                    "elements": [
-                        {
-                            "type": "image",
-                            "image_url": img_url,
-                            "alt_text": img_alt_text,
-                        },
-                        {"type": "mrkdwn", "text": message},
-                    ],
-                },
+            block_elements.insert(
+                0, {"type": "image", "image_url": img_url, "alt_text": img_alt_text}
             )
-        else:
-            self.blocks.append(
-                {
-                    "type": "context",
-                    "elements": [
-                        {"type": "mrkdwn", "text": message},
-                    ],
-                },
+
+        if tag_stakeholders and self.stakeholders:
+            block_elements.append(
+                {"type": "mrkdwn", "text": self.build_tag_stakeholder_message()}
             )
+
+        self.blocks.append(
+            {
+                "type": "context",
+                "elements": block_elements,
+            }
+        )
+
         self.update_block_message()
         if temp:
             self.blocks.pop()
 
-    def add_success_block(self) -> None:
-        """Add success message to block."""
+    def add_success_block(self, tag_stakeholders: bool = False) -> None:
+        """
+        Add success message to block.
+
+        Args:
+            tag_stakeholders (bool, optional): Whether to tag stakeholders in the message. Defaults to False.
+        """
+        block_elements = [{"type": "mrkdwn", "text": " *Job Successful* !! :tada: "}]
+
+        if tag_stakeholders and self.stakeholders:
+            block_elements.append(
+                {"type": "mrkdwn", "text": self.build_tag_stakeholder_message()}
+            )
+
         self.blocks.append(
             {
                 "type": "context",
-                "elements": [
-                    {"type": "mrkdwn", "text": " *Job Successful* !! :tada: "},
-                ],
-            },
+                "elements": block_elements,
+            }
         )
+
         self.update_block_message()
 
     def add_error_block(
-            self, error_message: str | None = None, notify: bool = False
+        self, error_message: str | None = None, tag_stakeholders: bool = False
     ) -> None:
         """
         Add error block to the Slack message block.
 
         Args:
-            error_message: plain error text message.
-            notify: True if you want to notify team members to fix the error,
-                else False.
+            error_message (str, optional): Plain error text message. Defaults to None.
+            tag_stakeholders (bool, optional): Whether to tag stakeholders in the message. Defaults to False.
         """
+        block_elements = [
+            {"type": "mrkdwn", "text": " *Job Unsuccessful* :disappointed_relieved: "}
+        ]
+
+        if error_message is not None:
+            block_elements.append(
+                {"type": "mrkdwn", "text": f" *Error Message* : {error_message} \n"}
+            )
+
+        if tag_stakeholders and self.stakeholders:
+            block_elements.append(
+                {"type": "mrkdwn", "text": self.build_tag_stakeholder_message()}
+            )
+
         self.blocks.append(
             {
                 "type": "context",
-                "elements": [
-                    {
-                        "type": "mrkdwn",
-                        "text": " *Job Unsuccessful* :disappointed_relieved: ",
-                    },
-                ],
-            },
+                "elements": block_elements,
+            }
         )
-        if error_message is not None:
-            self.blocks.append(
-                {
-                    "type": "context",
-                    "elements": [
-                        {
-                            "type": "mrkdwn",
-                            "text": f" *Error Message* : {error_message} ",
-                        },
-                    ],
-                },
-            )
-        if notify:
-            assert self.config.user1 is not None or self.config.user2 is not None
 
-            user_str = ""
-            if self.config.user1 is not None:
-                user_str += f"<@{self.config.user1}>"
-            if self.config.user2 is not None:
-                # Add a space before appending user2 if user_str already contains user1
-                if user_str:
-                    user_str += " "
-                user_str += f"<@{self.config.user2}>"
-
-            self.blocks.append(
-                {
-                    "type": "context",
-                    "elements": [
-                        {
-                            "type": "mrkdwn",
-                            "text": user_str,
-                        },
-                    ],
-                },
-            )
         self.update_block_message()
+
+    def build_tag_stakeholder_message(self) -> str:
+        """
+        Builds a message tagging all stakeholders, with each tag on a new line.
+
+        Returns:
+            str: The message containing the tags of all stakeholders, or an empty string if there are no stakeholders.
+        """
+        if not self.stakeholders:
+            return ""
+        else:
+            message = ""
+            for name, member_id in self.stakeholders.items():
+                message += f"<@{member_id}>\n"
+            return message
