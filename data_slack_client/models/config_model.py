572d680e126109cf9db7425ad3ef7d72a0fd617c
Humaid Mollah <73120385+hnm27@users.noreply.github.com>
2024-07-22 16:13:34 +0200

    update config to include only bot token

--- data_slack_client/models/config_model.py
+++ data_slack_client/models/config_model.py
@@ -1,5 +1,4 @@
 """Config model."""
-from typing import Optional
 
 from pydantic import BaseModel, constr
 
@@ -8,8 +7,6 @@ class SlackConfig(BaseModel):
     """Slack config data model."""
 
     bot_token: constr(min_length=1)  # type: ignore
-    user1: Optional[constr(min_length=1)]  # type: ignore
-    user2: Optional[constr(min_length=1)]  # type: ignore
 
 
 class ConfigModel(BaseModel):
